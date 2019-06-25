c33dd420c34a302044c58ccddf8e252fc4507abf
brime01 <yhy129852@gmail.com>
2019-06-25 17:17:25 +0900

    work src

---
+++ nullmasterjob/src/work/bean/vd/WorkDataBean.java
@@ -0,0 +1,21 @@
+package work.bean.vd;
+
+public class WorkDataBean {
+	private int jno;
+	private String jname;
+	
+	public int getJno() {
+		return jno;
+	}
+	public void setJno(int jno) {
+		this.jno = jno;
+	}
+	public String getJname() {
+		return jname;
+	}
+	public void setJname(String jname) {
+		this.jname = jname;
+	}
+	
+	
+}
